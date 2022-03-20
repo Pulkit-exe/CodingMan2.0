19126d7d33d8f5cee83b85d657a02b0dc3411ff1
Pulkit-exe <97144460+Pulkit-exe@users.noreply.github.com>
2022-03-20 10:14:42 +0530

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,82 +1,99 @@
+
+   
 import streamlit as st
-from random import choice
-st.set_page_config('CodingMan')
-st.header('Coding Man 2.0')
-st.subheader('Only 5-letter keywords included.....')
-l=['false','tuple','while','break','async','await','raise','yeild','count']                       #list of 5-aplhabet keyowrds in python!
-ans=choice(l)                                                                                     #the word to be found in game is selected and stored here
-n=len(ans)                                                                                        #length of word since we are taking 5-letter keyword only
-word='_ _ _ _ _ '                                                                                 #takes the input and update the correct letters 
-w_count=0                                                                                         #win count variable
-l_count=0                                                                                         #lose count variable
-chances=n+4                                                                                       #technically, we are giving 5 chances to user till hangwan dies                                                                                         #chances to play
-count=1                                                                                           #counter variable
 
-display=st.empty()                                                                                #for displaying the word variable
-display.markdown( f"<h1 style='text-align: center; color: green;'>{word}</h1>", unsafe_allow_html=True)
-                                              #for making list which stores inputs
+st.set_page_config( page_title = "CodingMan")
+#for page title
 
+lose = 0 #for tracking wrong inputs
+win = 0 #for tracking right inputs
 
-
-def win_meter(letter,ans):
-    global w_count
-    global l_count
-    if letter in ans:
-        w_count+=1
+def win_check(letter, text):
+    global lose
+    global win
+    if letter not in text:
+        lose += 1
     else:
-        l_count+=1
+        win += 1
 
-    if w_count==len(ans):
+    if win == len(text):
         st.balloons()
 
+
 def update(letter, word, text):
+    """ This function is to update the word after every input, to be displayed"""
+    text = list(text)
     word = list(word)
     letter = letter.lower()
 
-    for i in range (len(text)):
-        if letter==text[i]:
+    for i in range(len(text)):
+        if text[i] == letter:
             word[2*i] = letter
 
-    n_word = ""
+    new = ""
 
-    word = n_word.join(word)
-    display.markdown( f"<h1 style='text-align: center; color: green;'>{word}</h1>", unsafe_allow_html=True)
+    word = new.join(word)
+    u.markdown( f"<h1 style='text-align: center; color: red;'>{word}</h1>", unsafe_allow_html=True)
     return word
 
 
 
-tab=st.columns(2)                                                                                 #5 columns for input and 1 for hangman pics
-with tab[0]:
+st.markdown("<h1 style='text-align: center; color: royalblue;'>CodingMan</h1>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center; '>Customized Hangman For Python Programmers</h3>", unsafe_allow_html=True)
+
+u = st.empty() #for displaying the word
+
+col = st.columns(2)  #4 columns for input and 1 column for hangman in desktop version, this site is not responsive
+
+with col[0]:
     t1 = st.container()
-with tab[1]:
-    t6=st.empty()
+
+with col[1]:
+    t5 = st.empty()
 
 
-t6.image('hangmanpics/1.jpg')
+text = "tuple"
+word = "_ _ _ _ _ "
+limit = 0  #for attemted permissible inputs; which I took 4 more than the length of the word
+count = 1  #for counting the input
+letters = ['', '', '', '', '', '','','','','','','','','',''] #for storing inputs
 
+u.markdown( f"<h1 style='text-align: center; color: red;'>{word}</h1>", unsafe_allow_html=True)
+
+
+
+limit = len(text) + 4
+
+t5.image('hangmanpics/1.jpg')
+
+
+letters[0] = t1.text_input(f"Enter a letter:", max_chars = 1, key = 1 )
 
 j=0
-alpha=t1.text_input('Enter a letter:',max_chars=1,key=1)
+k=1
 while True:
-    if alpha and count!=chances and l_count!=4 and w_count!=len(ans):
+    if letters[j] and count!=chances and lose!=4 and win!=len(text):
         win_meter(alpha,ans)
         update(alpha,word,ans)
         j+=1
+        k+=1
         count+=1
-        alpha=t1.text_input('Enter a letter:', max_chars=1, key=j+1)
+        letters[j]=t1.text_input(f'Enter a letter:', max_chars=1, key=k)
     else:
         break
-        
-        
-if l_count == 1:
-    t6.image('hangmanpics/2.jpg')
-elif l_count == 2:
-    t6.image('hangmanpics/3.jpg')
-elif l_count == 3:
-    t6.image('hangmanpics/4.jpg')
-elif l_count == 4:
-    t6.image('hangmanpics/5.jpg')
 
+
+
+                                    
+
+if lose == 1:
+    t5.image('hangmanpics/2.jpg')
+elif lose == 2:
+    t5.image('hangmanpics/3.jpg')
+elif lose == 3:
+    t5.image('hangmanpics/4.jpg')
+elif lose == 4:
+    t5.image('hangmanpics/5.jpg')
 
 
 
